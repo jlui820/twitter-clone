89a781d7feaf30f10d30bfc2d7975006f535d410
Jeff <jlui820@gmail.com>
2020-09-10 12:12:04 -0700

    start twitter-clone app made sidebar page

---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      {/* Twitter icon */}
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
