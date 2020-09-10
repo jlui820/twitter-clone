29b5440f762196d0e4ccc1a6a9a2ad2280fca754
Jeff <jlui820@gmail.com>
2020-09-10 16:11:26 -0700

    add icons for sidebar and tweet button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>Hey Clever Programmers.. Lets build a twitter clone</h1>
 
       {/* Sidebar */}
+      <Sidebar />
 
       {/* Feed */}
 
